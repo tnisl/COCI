313ac69a314a25e481526479e39e40002f4f4053
Natick <memedead123@gmail.com>
2024-11-10 00:09:54 +0700

    Janjetina s

---
+++ 2020-2021/contest_4/Janjetina.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+#define fi first
+#define se second
+#define endl '\n'
+#define Bit(i,x) ((x>>i)&1)
+#define Flip(i,msk) (msk^(1<<i))
+#define TIME (1.0*clock()/CLOCKS_PER_SEC)
+
+
+//const
+const ll e=1e9+7;
+const ll inf=1e18;
+const int nmax=4e4;
+const int base=311;
+const int maxa=1e9;
+//
+
+
+void solve(){
+
+  
+}
+
+
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(0),cout.tie(0);
+    #ifndef ONLINE_JUDGE
+    freopen(".inp", "r", stdin);
+    freopen(".out", "w", stdout);
+    #endif
+  int test;
+  cin>>test;
+  while(test--)
+    solve();
+  return 0;
+}
